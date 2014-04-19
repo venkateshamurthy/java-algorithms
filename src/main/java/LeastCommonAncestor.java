aa669695ab3bc2f67a3f9907ded3d7055db9d5ef
vmurthy <ts_v_murthy@yahoo.com>
2014-04-19 20:37:47 +0000

    least common ancestor

--- src/main/java/LeastCommonAncestor.java
+++
@@ -1,7 +0,0 @@
-package com.github.venkateshamurthy.algorithms.tree;
-/**
- * A Class to find Common Ancestor of 2 nodes in a BST
- */
-public class LeastCommonAncestor<Integer> implements Visitor<Integer,Void,Boolean>{
-  
-}
