f154749a6d93f3ce288b0cedbaa2f507648deae6
vmurthy <ts_v_murthy@yahoo.com>
2024-10-21 13:54:48 +0530

    Some updates..

---
+++ java-algorithms-core/src/main/java/basics/Misc.java
@@ -0,0 +1,7 @@
+package basics;
+
+public class Student {
+    public static void main(String[]args){
+    System.out.println("a\t"+"aiml\t"+"3rd year");
+    System.out.println("b\t"+"ece \t"+"2nd year");
+}}
